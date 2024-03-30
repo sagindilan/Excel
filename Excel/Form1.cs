d5a8b2d8977be6c030ab42168c6d862a73d25913
sagindilan <sagindilan@gmail.com>
2024-03-30 22:40:47 +0300

    filedialog edit

--- Excel/Form1.cs
+++ Excel/Form1.cs
@@ -15,26 +15,23 @@ namespace Excel
         private void excelBtn_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            //openFileDialog.Filter = " |*.xlsx";
+            openFileDialog.Filter = " |*.xlsx";
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string excelYolu = openFileDialog.FileName;
                
                 OleDbConnection baglanti = new OleDbConnection($@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={excelYolu};Extended Properties='Excel 12.0 Xml;HDR=YES'");
-                
                 baglanti.Open();
 
                     DataTable dtExcel = baglanti.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
                     string sayfaAdi = dtExcel.Rows[0]["TABLE_NAME"].ToString();
-
                     OleDbCommand komut = new OleDbCommand($"Select * From [{sayfaAdi}]", baglanti);
-
                     OleDbDataAdapter da = new OleDbDataAdapter(komut);
-                    
-                        DataTable data = new DataTable();
-                        da.Fill(data); 
-                        dataGridView1.DataSource = data;
+
+                    DataTable data = new DataTable();
+                    da.Fill(data); 
+                    dataGridView1.DataSource = data;
                     
                 
             }
