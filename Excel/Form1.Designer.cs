e56a732c0a5ea45cf61e7f43e13e9602d7b5f484
sagindilan <sagindilan@gmail.com>
2024-03-30 22:13:41 +0300

    Add project files.

---
+++ Excel/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.excelBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // excelBtn
+            // 
+            this.excelBtn.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.excelBtn.Location = new System.Drawing.Point(300, 60);
+            this.excelBtn.Name = "excelBtn";
+            this.excelBtn.Size = new System.Drawing.Size(139, 32);
+            this.excelBtn.TabIndex = 0;
+            this.excelBtn.Text = "Dosya Seç";
+            this.excelBtn.UseVisualStyleBackColor = false;
+            this.excelBtn.Click += new System.EventHandler(this.excelBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(710, 283);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.excelBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button excelBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Excel/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void excelBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = " |*.xlsx";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string excelYolu = openFileDialog.FileName;
+               
+                OleDbConnection baglanti = new OleDbConnection($@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={excelYolu};Extended Properties='Excel 12.0 Xml;HDR=YES'");
+                
+                baglanti.Open();
+
+                    DataTable dtExcel = baglanti.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    string sayfaAdi = dtExcel.Rows[0]["TABLE_NAME"].ToString();
+
+                    OleDbCommand komut = new OleDbCommand($"Select * From [{sayfaAdi}]", baglanti);
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(komut);
+                    
+                        DataTable data = new DataTable();
+                        da.Fill(data); 
+                        dataGridView1.DataSource = data;
+                    
+                
+            }
+        }
+    }
+}
